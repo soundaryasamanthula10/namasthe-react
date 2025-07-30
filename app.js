50dd54918c2d0ff049050d446e2a7144c692ad0f
soundarya <soundaryasamanthula2410@gmail.com>
2025-07-30 14:21:58 +0530

    appjs

--- app.js
+++ app.js
@@ -1,33 +1,6 @@
-/**
- * 
- * <div id='parent'>
- * <div id='child'>
- * <h1></h1>
- * </div>
- * </div>
- */
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-const parent = React.createElement(
-    'div', 
-    {id:'parent'},
-    [React.createElement(
-        'div',
-        {id:'child'}, 
-        [React.createElement('h1',{},'Helo I am h1 tag'),React.createElement('h2',{},'I am h2 tag'),
-    ]),
-    React.createElement(
-        'div',
-        {id:'child'}, 
-        [React.createElement('h1',{},'Helo I am h1 tag'),React.createElement('h2',{},'I am h2 tag'),
-    ]),
-]);
-
-//JSX
-
-
-
-// const heading = React.createElement('h1',{id:"heading"},'Hello World from React!');
-
+const heading = React.createElement('h1', {}, "HELLO WORLD REACT");
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
+root.render(heading);
